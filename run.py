79f3ca59d9627cdf394b3df30ea91c1e484f4997
deepprakashnishad <deep.prakash.nishad@gmail.com>
2022-07-09 16:04:14 +0530

    Minor fix

--- run.py
+++ run.py
@@ -3,20 +3,35 @@ from flask_pymongo import PyMongo
 from mongoengine_jsonencoder import MongoEngineJSONEncoder
 from utils import my_constants as mconst
 
-def create_app(config_filename):
-    myapp = Flask(__name__)
-    myapp.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
-    myapp.config.from_object(config_filename)
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+app.config.from_object("config")
 
-    from app import api_bp
+from app import api_bp
+
+app.register_blueprint(api_bp, url_prefix='/api')
+
+from model import db, MongoEngineJSONEncoder
+app.json_encoder = MongoEngineJSONEncoder
+db.init_app(app)
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to Kite Autotrader'
+
+# def create_app(config_filename):
+#     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+#     app.config.from_object(config_filename)
+
+#     from app import api_bp
     
-    myapp.register_blueprint(api_bp, url_prefix='/api')
+#     app.register_blueprint(api_bp, url_prefix='/api')
 
-    from model import db, MongoEngineJSONEncoder
-    myapp.json_encoder = MongoEngineJSONEncoder
-    db.init_app(myapp)
+#     from model import db, MongoEngineJSONEncoder
+#     app.json_encoder = MongoEngineJSONEncoder
+#     db.init_app(myapp)
 
-    return myapp
+#     return app
 
 if __name__ == "__main__":
     app = create_app("config")
