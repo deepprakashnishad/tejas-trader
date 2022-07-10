a85157e43c6471c2f83541a4b5b80dddd22b2def
deepprakashnishad <deep.prakash.nishad@gmail.com>
2022-07-10 10:28:45 +0530

    Added CORS

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 from mongoengine_jsonencoder import MongoEngineJSONEncoder
 from utils import my_constants as mconst
