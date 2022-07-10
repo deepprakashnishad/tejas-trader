cfee6f918320166abe88e123a482a113448eedd8
deepprakashnishad <deep.prakash.nishad@gmail.com>
2022-07-10 15:29:49 +0530

    Initial commit

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ env = mconst.ENV;
 if env=="PROD":
     db_url = mconst.PROD_DB_URL
     db_name = mconst.PROD_DB_NAME
+    MONGOALCHEMY_REPLICA_SET = mconst.PROD_REPLICA_SET
 else:
     db_url = mconst.DB_URL
     db_name = mconst.DB_NAME
 
 MONGOALCHEMY_DATABASE=db_name
 MONGOALCHEMY_CONNECTION_STRING=db_url
-MONGOALCHEMY_REPLICA_SET = mconst.PROD_REPLICA_SET
