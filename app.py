29666bbf7a3d9ac33639b056340264005acd2389
Deep Prakash Nishad <deep.prakash.nishad@gmail.com>
2023-03-14 16:26:33 +0530

    Removed ta-lib dependents

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.UserResource import UserResource
-from resources.backtest_resource import BacktestResource
+# from resources.backtest_resource import BacktestResource
 from resources.daily_selection import DailySelectionResource
-from resources.deploy_strategy_resource import DeployStrategyResource
+# from resources.deploy_strategy_resource import DeployStrategyResource
 from resources.instrument_resource import InstrumentsResource
-from resources.operator_resource import OperatorResource, OperatorsResource
-from resources.screener_resource import ScreenerResource, ScreenersResource
-from resources.strategy_resource import StrategyResource, StrategiesResource
-from resources.technical_resource import TechnicalResource, TechnicalsResource
-from resources.authentication_resource import AuthenticationResource
+# from resources.operator_resource import OperatorResource, OperatorsResource
+# from resources.screener_resource import ScreenerResource, ScreenersResource
+# from resources.strategy_resource import StrategyResource, StrategiesResource
+# from resources.technical_resource import TechnicalResource, TechnicalsResource
+# from resources.authentication_resource import AuthenticationResource
 from resources.seed_resource import SeedResource
 from resources.derivative_analysis_resource import DerivativeAnalysisResource, DetailedDerivativeAnalysisResource, DataLoaderAnalysisResource, IndexOptionChainResource
 # from resources.index_resource import IndexOptionChainResource
@@ -25,29 +25,29 @@ api = Api(api_bp)
 # Route
 api.add_resource(UserResource, '/user')
 
-api.add_resource(StrategyResource, '/strategy', '/strategy/<string:id>')
+# api.add_resource(StrategyResource, '/strategy', '/strategy/<string:id>')
 
-api.add_resource(StrategiesResource, '/strategies')
+# api.add_resource(StrategiesResource, '/strategies')
 
-api.add_resource(ScreenerResource, '/screener', '/screener/<string:id>')
+# api.add_resource(ScreenerResource, '/screener', '/screener/<string:id>')
 
-api.add_resource(ScreenersResource, '/screeners')
+# api.add_resource(ScreenersResource, '/screeners')
 
-api.add_resource(TechnicalResource, '/technical', '/technical/<string:_id>')
+# api.add_resource(TechnicalResource, '/technical', '/technical/<string:_id>')
 
 api.add_resource(AuthenticationResource, '/authentication')
 
-api.add_resource(OperatorResource, '/operator', '/operator/<string:_id>')
+# api.add_resource(OperatorResource, '/operator', '/operator/<string:_id>')
 
-api.add_resource(TechnicalsResource, '/technicals')
+# api.add_resource(TechnicalsResource, '/technicals')
 
-api.add_resource(OperatorsResource, '/operators')
+# api.add_resource(OperatorsResource, '/operators')
 
 api.add_resource(InstrumentsResource, '/instruments')
 
-api.add_resource(DeployStrategyResource, '/deploy')
+# api.add_resource(DeployStrategyResource, '/deploy')
 
-api.add_resource(BacktestResource, '/backtest')
+# api.add_resource(BacktestResource, '/backtest')
 
 api.add_resource(DerivativeAnalysisResource, '/derivativeAnalysisResult')
 
--- render_build.sh
+++ render_build.sh
@@ -1,12 +1,1 @@
-sudo apt-get -y install gcc build-essential
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz \
-  && tar -xzf ta-lib-0.4.0-src.tar.gz \
-  && rm ta-lib-0.4.0-src.tar.gz \
-  && cd ta-lib/ \
-  && ./configure --prefix=/usr \
-  && make \
-  && make install \
-  && cd ~ \
-  && rm -rf ta-lib/ \
-  && pip install ta-lib \
-  && pip install requirement.txt
+pip install requirement.txt
