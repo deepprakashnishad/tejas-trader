16cf36f4018f066b3fda03ebed4ef99d289ae078
Deep Prakash Nishad <deep.prakash.nishad@gmail.com>
2023-03-14 15:33:52 +0530

    removed sudo command

--- render_build.sh
+++ render_build.sh
@@ -2,8 +2,8 @@ wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzf ta-lib-0.4.0-src.tar.gz
 cd ta-lib/
 
-sudo ./configure
-sudo make
-sudo make install
-
+./configure
+make
+make install
+cd ..
 pip install ta-lib
