11d4651800cda70b0b5b75decc8b63397cbfd014
Deep Prakash Nishad <deep.prakash.nishad@gmail.com>
2023-03-14 15:26:47 +0530

    Added build file for ta-lib

---
+++ render_build.sh
@@ -0,0 +1,9 @@
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+
+sudo ./configure
+sudo make
+sudo make install
+
+pip install ta-lib
