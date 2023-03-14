d37f30faa4bf5f9cda1f4838f83b468b1140a7cb
Deep Prakash Nishad <deep.prakash.nishad@gmail.com>
2023-03-14 16:12:05 +0530

    Modified build file

--- render_build.sh
+++ render_build.sh
@@ -1,12 +1,12 @@
 sudo apt-get -y install gcc build-essential
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz \
-  && sudo tar -xzf ta-lib-0.4.0-src.tar.gz \
-  && sudo rm ta-lib-0.4.0-src.tar.gz \
+  && tar -xzf ta-lib-0.4.0-src.tar.gz \
+  && rm ta-lib-0.4.0-src.tar.gz \
   && cd ta-lib/ \
-  && sudo ./configure --prefix=/usr \
-  && sudo make \
-  && sudo make install \
+  && ./configure --prefix=/usr \
+  && make \
+  && make install \
   && cd ~ \
-  && sudo rm -rf ta-lib/ \
+  && rm -rf ta-lib/ \
   && pip install ta-lib \
   && pip install requirement.txt
